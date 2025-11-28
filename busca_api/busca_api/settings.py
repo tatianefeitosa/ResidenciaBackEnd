4bcf6d04c72182de5f121f18e984c7122b27d215
Tatiane Feitosa <tatianegomes2312@gmail.com>
2025-11-28 00:49:10 -0300

    Att secret key

--- busca_api/busca_api/settings.py
+++ busca_api/busca_api/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-local-placeholder-key")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 

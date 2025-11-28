9d70d84c344085a4cd173e7a37c6489c78b410e5
Tatiane Feitosa <tatianegomes2312@gmail.com>
2025-11-28 04:40:09 -0300

    Remove Whitenoise

--- busca_api/busca_api/wsgi.py
+++ busca_api/busca_api/wsgi.py
@@ -10,9 +10,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'busca_api.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))

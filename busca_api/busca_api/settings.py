dec9b8fab420ad5701fbfc0663a04726dc1502a3
tiagocomth <thiago8jesusaraujo@gmail.com>
2025-11-28 23:12:26 -0300

    mudancas para render

--- busca_api/busca_api/settings.py
+++ busca_api/busca_api/settings.py
@@ -27,7 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = ["*"]
+
 
 
 
